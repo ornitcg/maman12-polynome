f226cefacc59f779df0d28122793ce32856c1063
OCG <40301816+ornitcg@users.noreply.github.com>
2021-03-29 22:48:06 +0300

    first commit

---
+++ src/Monomial.java
@@ -0,0 +1,48 @@
+
+
+
+public class Monomial implements Comparable{
+
+    private double _coefficient;
+    private Integer _power;
+
+
+    public Monomial(){
+        _coefficient = 0;
+        _power = 0;
+    }
+
+
+    public Monomial(double coeffitient, int power){
+        _coefficient = coeffitient;
+        _power = power;
+    }
+
+
+
+    public String toString(Monomial monomial){
+        String coefStr = "",  powerStr = "";
+        if (_coefficient != 1)
+            coefStr = String.valueOf(_coefficient);
+        if (_power ==1)
+            powerStr = "x";
+        else if (_power>1)
+            powerStr = "x^"+ _power;
+        return coefStr + powerStr;
+    }
+
+
+
+    public int compareTo(Object monomial) {
+        return _power.compareTo(((Monomial)monomial).get_power());
+    }
+
+
+    private int get_power() {
+        return _power;
+    }
+
+    public double get_coefficient() {
+        return _coefficient;
+    }
+}
---
+++ src/Polynom.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.lang.String;
+
+public class Polynome {
+    ArrayList<Monomial> _polynome;
+
+
+    public Polynom(double[] coeffitientArr,int[] powerArr){
+        buildPolynome();
+        sortPolynome();
+        //throws exception of different lengths
+    }
+
+
+    private void sortPolynome() {
+    }
+
+    private void buildPolynome() {
+    }
+
+    public Polynome plus(Polynome polynome){
+
+    }
+
+    public Polynome minus(Polynome polynome){
+
+    }
+
+    public Polynome derivative(Polynome polynome){
+
+    }
+
+    public String toString(Polynome polynome){
+
+    }
+
+    public Boolean equals(Polynome polynome){
+
+    }
+
+
+}
